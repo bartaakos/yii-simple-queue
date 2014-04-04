8dbb24f69867780452fd26e8ff57b5f878d6eb67
Barta Akos <akos@netpositive.hu>
2014-04-04 16:20:04 +0200

    added associated_id to the queue

--- migrations/m140404_122834_create_table.php
+++ migrations/m140404_122834_create_table.php
@@ -5,16 +5,19 @@ class m140404_122834_create_table extends CDbMigration
     public function up()
     {
         $this->execute(
-            "CREATE TABLE IF NOT EXISTS `queue` (
-              `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-              `type` tinyint(2) unsigned NOT NULL,
-	          `associated_id` INT UNSIGNED NULL,
-              `data` text NOT NULL,
-              `create_time` datetime DEFAULT NULL,
-              PRIMARY KEY (`id`),
-              KEY `type` (`type`),
-              KEY `create_time` (`create_time`)
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;"
+            "CREATE TABLE `queue` (
+                `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+                `type` TINYINT(2) UNSIGNED NOT NULL,
+                `associated_id` INT UNSIGNED NULL,
+                `data` TEXT NOT NULL,
+                `create_time` DATETIME NULL DEFAULT NULL,
+                PRIMARY KEY (`id`),
+                INDEX `type` (`type`),
+                INDEX `create_time` (`create_time`),
+                INDEX `associated_id` (`associated_id`)
+            )
+            COLLATE='utf8_general_ci'
+            ENGINE=InnoDB;"
         );
     }
 
